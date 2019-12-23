aaec4026ed41ff68ab065530c77ab5f39b20b57d
linrencai <1013188634@qq.com>
2019-12-23 22:22:49 +0800

    2

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="login-wrap">
     <el-form class="login-form" label-position="top" label-width="80px" :model="formdata">
-      <h2>用户登录</h2>
-      <el-form-item label="用户名">
-        <el-input v-model="formdata.admAccount"></el-input>
+      <h2>管理员登录</h2>
+      <el-form-item>
+        <el-input placeholder="请输入账号" v-model="formdata.admAccount"></el-input>
       </el-form-item>
-      <el-form-item label="密码">
-        <el-input v-model="formdata.admPassword"></el-input>
+      <el-form-item>
+        <el-input placeholder="请输入密码" v-model="formdata.admPassword"></el-input>
       </el-form-item>
-      <el-form-item label="验证码">
-        <el-input v-model="formdata.verificationCode"></el-input>
-        <!-- <img src="127.0.0.1:8080/verificationCode/getCode" alt="点击刷新验证码" /> -->
+      <el-form-item>
+        <el-input placeholder="请输入验证码" v-model="formdata.verificationCode">
+          <template slot="append">
+            <img :src="url" alt="点击刷新验证码" @click="getCode()"/>
+          </template>
+        </el-input>
+
         <!-- <div class="img" v-html="url">{{url}}</div>
-        <img :src="url" alt=""> -->
+        <img :src="url" alt="">-->
       </el-form-item>
       <el-button @click.prevent="handleLogin()" class="login-btn" type="primary">登录</el-button>
     </el-form>
@@ -21,50 +25,56 @@
 
 <script>
 export default {
-  data () {
+  data() {
     return {
       formdata: {
-        admAccount: '',
-        admPassword: '',
-        verificationCode: ''
+        admAccount: "",
+        admPassword: "",
+        verificationCode: ""
       },
-      url: ''
-    }
+      url: ""
+    };
   },
-  created () {
-    // this.grtCode()
+  created() {
+    this.getCode();
   },
   methods: {
-    async handleLogin () {
-      const res = await this.$http.post('admin/adminLogin', this.formdata)
-      console.log(res)
+    async handleLogin() {
+      const res = await this.$http.post("admin/adminLogin", this.formdata);
+      console.log(res);
       const {
         data,
         meta: { retCode, retMsg }
-      } = res.data
+      } = res.data;
       // 登陆成功
       // 1.跳转home
-      if (retCode === 'CODE0002') {
+      if (retCode == "ADMIN0000") {
         // 保存token值
-        localStorage.setItem('token', data.token)
+        localStorage.setItem("token", data.token);
         // 2.提示成功
-        this.$message.success(retMsg)
+        this.$message.success(retMsg);
         // 跳转页面
-        this.$router.push({ name: 'home' })
+        this.$router.push({ name: "home" });
       } else {
         // 登陆不成功
         // 1.提示消息
-        this.$message.warning(retMsg)
+        this.$message.warning(retMsg);
       }
+    },
+    async getCode() {
+      const imageBuffer = await this.$http.get("verificationCode/getCode", {
+        responseType: "arraybuffer"
+      });
+      let bytes = new Uint8Array(imageBuffer.data);
+      let data = "";
+      let len = bytes.byteLength;
+      for (let i = 0; i < len; i++) {
+        data += String.fromCharCode(bytes[i]);
+      }
+      this.url = "data:image/png;base64," + window.btoa(data);
     }
-    // async grtCode () {
-    //   const res = await this.$http.get('verificationCode/getCode')
-    //   console.log(res)
-    //   const { data } = res.data
-    //   this.url = 'data:image/jpeg;base64,' + data
-    // }
   }
-}
+};
 </script>
 
 <style>
@@ -83,9 +93,5 @@ export default {
 }
 .login-wrap .login-btn {
   width: 100%;
-}
-.img {
-    display: inline-block;
-    height: 100%;
 }
 </style>
