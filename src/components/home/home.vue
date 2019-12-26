ea7d914b00473c9570c3a6f40e3be3ab52de15d3
linrencai <1013188634@qq.com>
2019-12-26 23:09:15 +0800

    6

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,119 +1,176 @@
 <template>
-<el-container class="container">
+  <el-container class="container">
     <el-header class="header">
-        <el-row>
-            <el-col :span="4">
-                
-            </el-col>
-            <el-col :span="18" class="middle">
-                <h2>师生在线教育系统</h2>
-            </el-col>
-            <el-col :span="2">
-                <div class="grid-content bg-purple">
-                    <a href="#" class="loginout" @click.prevent="handleSignout()">退出登录</a>
-                </div>
-            </el-col>
-        </el-row>
+      <el-row>
+        <el-col :span="3" class="middle">
+          <h2>{{role}}</h2>
+        </el-col>
+        <el-col :span="18" class="middle">
+          <h2>师生在线教育系统</h2>
+        </el-col>
+        <el-col :span="3">
+          <div class="grid-content bg-purple" style="text-align:center;">
+            <a href="#" class="loginout" @click.prevent="handleSignout()">退出登录</a>
+          </div>
+        </el-col>
+      </el-row>
     </el-header>
     <el-container>
-        <el-aside width="200px" class="aside">
-            <!-- el-menu侧边栏导航 -->
-            <!-- 开启路由 -->
-            <el-menu
-            :router = "true"
-            :unique-opened = "true"
-            >
-            <!-- 遍历出侧边栏菜单 -->
-                <el-submenu :index=" ''+item1.order" v-for="(item1,i) in menuList" :key="i">
-                    <template slot="title">
-                        <i class="el-icon-location"></i>
-                        <span> {{item1.authName}} </span>
-                    </template>
-                    <el-menu-item :index=" ''+item2.path" v-for="(item2,i2) in item1.children" :key="i2">
-                        <i class="el-icon-circle-check"></i>
-                        <span> {{item2.authName}} </span>
-                    </el-menu-item>
-                </el-submenu>
-                
-            </el-menu>
-        </el-aside>
-        <el-main class="main">
-            <router-view></router-view>
-        </el-main>
+      <el-aside width="200px" class="aside">
+        <!-- el-menu侧边栏导航 -->
+        <!-- 开启路由 -->
+        <el-menu :router="true" :unique-opened="true" background-color="#545c64">
+          <!-- 遍历出侧边栏菜单 -->
+          <el-submenu :index=" ''+item1.order" v-for="(item1,i) in menuList" :key="i">
+            <template slot="title">
+              <!-- <i class="el-icon-location"></i> -->
+              <span>{{item1.authName}}</span>
+            </template>
+            <el-menu-item :index=" ''+item2.path" v-for="(item2,i2) in item1.children" :key="i2">
+              <!-- <i class="el-icon-circle-check"></i> -->
+              <span>{{item2.authName}}</span>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <el-main class="main">
+        <router-view></router-view>
+      </el-main>
     </el-container>
-</el-container>
+  </el-container>
 </template>
 
 <script>
 export default {
-    // 组件渲染之前检测是否有token值
-    // beforeCreate() {
-        // const token = localStorage.getItem('token')
-        // // 没有token 值就跳转登录页面
-        // if(!token){
-        //     this.$router.push( {name: 'login'})
-        // }
-    // },
-    data(){
-        return{
-            menuList:[]
-        }
+  // 组件渲染之前检测是否有token值
+  // beforeCreate() {
+  //     const token = localStorage.getItem('token')
+  //     this.token = token
+  //     console.log(token)
+  //     // 没有token 值就跳转登录页面
+  //     if(!token){
+  //         this.$router.push( {name: 'login'})
+  //     }
+  // },
+  data() {
+    return {
+      menuList: [],
+      token: "",
+      role: ""
+    };
+  },
+  mounted() {
+    this.getMenus();
+  },
+  methods: {
+    // 设置动态创建侧边栏
+    async getMenus() {
+      // const res = await this.$http.get(`menus`)
+      const token = localStorage.getItem("token");
+      // const token = this.token
+      if (token == "ADMIN0000") {
+        this.role = "管理员";
+        this.menuList = [
+          {
+            authName: "公告管理",
+            order: 1,
+            children: [
+              {
+                authName: "公告管理",
+                path: "/orders"
+              }
+            ]
+          },
+          {
+            authName: "注册审核",
+            order: 2,
+            children: [
+              {
+                authName: "学生审核",
+                path: "/"
+              },
+              {
+                authName: "教师审核",
+                path: "/"
+              }
+            ]
+          },
+          {
+            authName: "系统提问",
+            order: 3,
+            children: [
+              {
+                authName: "学生提问",
+                path: "/"
+              },
+              {
+                authName: "教师提问",
+                path: "/"
+              }
+            ]
+          },
+          {
+            authName: "信息评论管理",
+            order: 4,
+            children: [
+              {
+                authName: "信息评论管理",
+                path: "/"
+              }
+            ]
+          }
+        ];
+      }
     },
-    created(){
-        this.getMenus()
-    },
-    methods: {
-        // 设置动态创建侧边栏
-        async getMenus(){
-            const res = await this.$http.get(`menus`)
-            // console.log(res)
-            this.menuList = res.data.data
-        },
-        // 退出登录功能
-        handleSignout(){
-            // 清除token
-            localStorage.clear()
-            // 提示信息
-            this.$message.success('退出成功')
-            //跳转到login组件
-            this.$router.push({name: 'login'})
-        }
+    // 退出登录功能
+    handleSignout() {
+      // 清除token
+      localStorage.clear();
+      // 提示信息
+      this.$message.success("退出成功");
+      //跳转到login组件
+      this.$router.push({ name: "login" });
     }
-}
+  }
+};
 </script>
 
 <style>
+.logo img {
+  height: 100%;
+  width: auto;
+}
 .container {
-    height: 100%;
+  height: 100%;
 }
 
 .aside {
-    background-color: #D3DCE6;
-    color: #333;
-    text-align: center;
+  background-color: #d3dce6;
+  color: #333;
+  text-align: center;
 }
 
 .main {
-    background-color: #E9EEF3;
-    color: #333;
+  background-color: #e9eef3;
+  color: #333;
 }
 
 .header {
-    background-color: #b3c0d1;
+  background-color: #b3c0d1;
 }
 
 .middle {
-    text-align: center;
-    line-height: 100%;
+  text-align: center;
+  line-height: 100%;
 }
 
 .loginout {
-    line-height: 60px;
-    text-decoration: none;
-    color: #000;
+  line-height: 60px;
+  text-decoration: none;
+  color: #000;
 }
 
 .loginout:hover {
-    color: red;
+  color: red;
 }
 </style>
--- src/components/order/order.vue
+++ src/components/order/order.vue
@@ -1,85 +1,57 @@
 <template>
-<el-card>
-    <my-bread level1="订单管理" level2="订单列表"></my-bread>
+  <el-card>
+    <my-bread level1="公告管理"></my-bread>
     <el-table :data="list" style="width: 100%">
-        <el-table-column type="index" label="#" width="40"></el-table-column>
-        <el-table-column prop="order_number" label="订单编号" width="180"></el-table-column>
-        <el-table-column prop="order_price" label="订单价格"></el-table-column>
-        <el-table-column prop="order_pay" label="是否付款" width="80">
-            <template slot-scope="scope">
-                <el-tag v-if="scope.row.order_pay==='0'">已付款</el-tag>
-                <el-tag type="danger" v-if="scope.row.order_pay==='1'">未付款</el-tag>
-            </template>
-        </el-table-column>
-        <el-table-column prop="is_send" label="是否发货" width="100"></el-table-column>
-        <el-table-column label="下单时间" width="90">
-            <template slot-scope="scope">{{scope.row.create_time|fmtdata}}</template>
-        </el-table-column>
-        <el-table-column label="操作" width="180">
-            <template slot-scope="scope">
-                <el-button type="primary" icon="el-icon-edit" circle size="mini" plain @click="showEditdia(scope.row)"></el-button>
-            </template>
-        </el-table-column>
+      <el-table-column prop="id" label="消息编号" width="80" align="center"></el-table-column>
+      <el-table-column prop="title" label="消息标题" width="260" align="center"></el-table-column>
+      <el-table-column prop="content" label="消息内容" width="500" align="center"></el-table-column>
+      <el-table-column prop="issueTime" label="发布时间" width="240" align="center"></el-table-column>
+      <el-table-column prop="adminId" label="发布者id" width="80" align="center"></el-table-column>
     </el-table>
-    <!-- 点击修改订单的弹窗 -->
-    <el-dialog title="修改订单地址" :visible.sync="dialogFormVisible">
-      <el-form :model="form">
-        <el-form-item label="省市区" label-width="120px">
-          <el-cascader
-            expand-trigger="hover"
-            clearable
-            :options="catlist"
-            v-model="selectedOptions"
-          ></el-cascader>
-        </el-form-item>
-        <el-form-item label="详细地址" label-width="120px">
-          <el-input v-model="form.address" autocomplete="off"></el-input>
-        </el-form-item>
-      </el-form>
-      <div slot="footer" class="dialog-footer">
-        <el-button @click="dialogFormVisible = false">取 消</el-button>
-        <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
-      </div>
-    </el-dialog>
-</el-card>
+  </el-card>
 </template>
 
 <script>
-import catlist from '@/assets/city_data2017_element.js'
+import catlist from "@/assets/city_data2017_element.js";
 export default {
-
-data(){
-    return{
-        list:[],
-        //弹窗de数据
-        dialogFormVisible:false,
-        form:{
-            address:""
-        },
-        //级联选择器的数据
-        catlist:[],
-        selectedOptions:[]
+  data() {
+    return {
+      list: [{
+          id:"1",
+          title:"测试标题",
+          content:"测试内容",
+          issueTime:"2019-12-18 23:47:32",
+          adminId:"001"
+      },{
+         id:"2",
+          title:"测试标题",
+          content:"测试内容",
+          issueTime:"2019-12-18 23:47:32",
+          adminId:"001" 
+      },{
+          id:"3",
+          title:"测试标题",
+          content:"测试内容",
+          issueTime:"2019-12-18 23:47:32",
+          adminId:"001"
+      }
+      ],
+    };
+  },
+  methods: {
+    
+    // 获取列表数据
+    async getMsgList() {
+      const res = await this.$http.post("sysMsg/querySysMsg");
+      console.log(res);
+    //   this.list = res.data.data.goods;
     }
-},
-methods:{
-    // 弹窗
-    showEditdia(){
-        this.catlist = catlist
-        this.dialogFormVisible = true
-    },
-    //获取列表shuju
-    async getOrderList(){
-        const res = await this.$http.get(`orders?pagenum=1&pagesize=10`)
-        console.log(res)
-        this.list = res.data.data.goods
-    }
-},
-created(){
-    this.getOrderList()
-}
-}
+  },
+  created() {
+    this.getMsgList();
+  }
+};
 </script>
 
 <style>
-
 </style>
